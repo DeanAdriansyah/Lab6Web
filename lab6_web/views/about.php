c1b0557a90fc222373a476a70382e10ff3e99843
D-yan <93472471+DeanAdriansyah@users.noreply.github.com>
2023-04-19 16:33:21 +0700

    Add files via upload

---
+++ lab6_web/views/about.php
@@ -0,0 +1,6 @@
+<div class="w-full p-5 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700" style="background-color:rgb(135, 206, 235,0.6);">
+  <img class="w-24 h-24 m-auto mb-4 rounded-full shadow-lg" src="https://avatars.githubusercontent.com/u/93472471?s=400&u=f3dc49a0d0a24aba75623cecd237c3dd9a4b6627&v=4" alt="Bonnie image" />
+  <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white" style="text-shadow: 3px 2px 1px grey;">Dean Adriansyah Asy'ari</title>
+</head></h5>
+  <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsum officia obcaecati soluta magni, perspiciatis officiis rerum quod non est nam reiciendis, deleniti animi placeat, accusamus aliquam odio et eaque quae maxime unde architecto similique ad. Qui dolore facilis eum veritatis sunt accusantium vitae, dicta provident nihil, dolorum repellat nobis!</p>
+</div>
